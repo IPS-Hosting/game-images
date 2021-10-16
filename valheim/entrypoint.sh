8c6d89b06836a8f80182afabad43147dd1e4a6e5
Pascal Sthamer <sthamer.pascal@gmail.com>
2021-10-16 15:22:20 +0200

    fix: set Valheim and ValheimPlus env variables
    Signed-off-by: Pascal Sthamer <sthamer.pascal@gmail.com>

--- valheim/entrypoint.sh
+++ valheim/entrypoint.sh
@@ -41,8 +41,24 @@ function update() {
 }
 
 function start() {
+	# Set ENV variables required by ValheimPlus, if it is installed.
+	# https://github.com/valheimPlus/ValheimPlus/blob/799a52a225487cc52e01a3cd77b37aa0e50be048/resources/unix/start_server_bepinex.sh
+    if [ -d "/home/ips-hosting/BepInEx" ]; then
+		export DOORSTOP_ENABLE=TRUE
+		export DOORSTOP_INVOKE_DLL_PATH=/home/ips-hosting/BepInEx/core/BepInEx.Preloader.dll
+		export DOORSTOP_CORLIB_OVERRIDE_PATH=/home/ips-hosting//unstripped_corlib
+		export LD_LIBRARY_PATH=/home/ips-hosting/doorstop_libs:$LD_LIBRARY_PATH
+		export LD_PRELOAD=libdoorstop_x64.so:$LD_PRELOAD
+		export DYLD_LIBRARY_PATH=/home/ips-hosting/doorstop_libs
+		export DYLD_INSERT_LIBRARIES=/home/ips-hosting/doorstop_libs/libdoorstop_x64.so
+	fi
+
+    # Set ENV variables required by Valheim server.
+	export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+	export SteamAppId=892970
+
 	local start_command="./valheim_server.x86_64 -port ${GAME_PORT:-2456} -name '${NAME:-A Valheim server}' -world '${WORLD:-Dedicated}' -password '${PASSWORD:-secret}' -savedir '/home/ips-hosting/data'"
-	
+
 	cd /home/ips-hosting
 	echo "$start_command"
 	eval "$start_command"
